9ff23e50c2bdbb52d816fd9872ef20ae8fbe273c
Wizuki <top4ikzero05@gmail.com>
2026-04-28 22:48:05 +0300

    ну да, это коммит

--- sort_all.py
+++ sort_all.py
@@ -11,20 +11,29 @@ def give_n_best(valid_words: dict[str, list[str]], n):
     max_familiarity = 2.356011
 
     vec_valid_words = []
+    re_kana = re.compile(r'[\u3040-\u309F]')
+    re_kanji = re.compile(r'[\u4E00-\u9FFF]')
+    labse_model = get_labse_model()
     for k, v in valid_words.items():
+        vecs = labse_model.encode(v, convert_to_numpy=True)
+
+        word_infos = []
+        
         for word in v:
-            labse_model = get_labse_model()
 
-            vec = labse_model.encode(word, convert_to_numpy=True)
-            word_info = np.array([len(word),
-                         sum([len(sym) for sym in re.findall(r'[\u3040-\u309F]+', word)]),
-                         sum([len(sym) for sym in re.findall(r'[\u4E00-\u9FFF]+', word)])])
-            input_data = np.hstack([vec, word_info]).reshape(1, -1)
+            word_infos.append([
+                len(word),
+                len(re_kana.findall(word)),
+                len(re_kanji.findall(word))
+            ])
 
-            predict = model.predict(input_data)
-            und_predict = max_familiarity + 1 - np.exp(predict)
+        input_data = np.column_stack([vecs, np.array(word_infos)])
 
-            vec_valid_words.append((und_predict, word, k))
+        predict = model.predict(input_data)
+        und_predict = max_familiarity + 1 - np.exp(predict)
+
+        for i, w_score in enumerate(und_predict):
+            vec_valid_words.append((w_score, v[i], k))
 
 
     vec_valid_words.sort(reverse=True)
